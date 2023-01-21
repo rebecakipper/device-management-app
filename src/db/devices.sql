dc4dc86e9a878e36c2d04e3f61c5b10c916d4c96
Rebeca Kipper <rebecakipper@gmail.com>
2023-01-21 22:33:40 +0100

    setup and seeds database

--- src/db/devices.sql
+++ src/db/devices.sql
@@ -1,8 +1,14 @@
+DROP TABLE IF EXISTS owners CASCADE;
+DROP TABLE IF EXISTS devices CASCADE;
+DROP TABLE IF EXISTS ownerships CASCADE;
+DROP TYPE IF EXISTS DEVICE_TYPE;
+
+CREATE TYPE DEVICE_TYPE AS ENUM('Laptop','Camera','Tablet','Smartphone');
+
 CREATE TABLE devices(
     id SERIAL PRIMARY KEY,
     device_name VARCHAR(255) NOT NULL CHECK(device_name !=''),
-    device_type VARCHAR(255) NOT NULL CHECK(device_type !=''),
-    device_owner_id VARCHAR(255) NOT NULL CHECK(device_owner_id !='') REFERENCES owners(id),
+    device_type DEVICE_TYPE NOT NULL,
     available_space INTEGER NOT NULL,
     battery_status INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -11,6 +17,39 @@ CREATE TABLE devices(
 CREATE TABLE owners(
     id SERIAL PRIMARY KEY,
     owner_name VARCHAR(255) NOT NULL CHECK(owner_name != ''),
-    devices INTEGER[] REFERENCES devices(id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
+
+ CREATE TABLE ownerships(
+    id SERIAL PRIMARY KEY,
+    owner_id  INTEGER NOT NULL REFERENCES owners(id),
+    device_id INTEGER NOT NULL REFERENCES devices(id), 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );   
+
+INSERT INTO 
+    devices (device_name, device_type, available_space, battery_status)
+VALUES 
+    ('A', 'Laptop', 360, 100),
+    ('B', 'Tablet', 40, 97),
+    ('C', 'Camera', 2360, 70),
+    ('D', 'Smartphone', 540, 40),
+    ('E', 'Laptop',50, 10);
+
+INSERT INTO 
+    owners (owner_name)
+VALUES 
+    ('Bob'),
+    ('John'),
+    ('Mark'),
+    ('Arnold'),
+    ('Elliot');
+
+INSERT INTO 
+    ownerships (owner_id,device_id)
+VALUES 
+    (1,1),
+    (1,2),
+    (2,3),
+    (4,5);
+
