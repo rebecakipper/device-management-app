47a349354d711e85f361bffeddb3feb455e6d530
Rebeca Kipper <rebecakipper@gmail.com>
2023-01-23 18:38:03 +0100

    redesigns deviceCard  component with device props

--- src/components/DeviceCard.tsx
+++ src/components/DeviceCard.tsx
@@ -2,19 +2,20 @@ import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
+
 import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
+
 import Avatar from "@mui/material/Avatar";
 import IconButton, { IconButtonProps } from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 
+import DeviceHubIcon from "@mui/icons-material/DeviceHub";
+import type { Device } from "@/types";
+import EditIcon from "@mui/icons-material/Edit";
+import Link from "next/link";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 interface ExpandMoreProps extends IconButtonProps {
     expand: boolean;
 }
@@ -30,7 +31,9 @@ const ExpandMore = styled((props: ExpandMoreProps) => {
     }),
 }));
 
-export default function DeviceCard() {
+type Props = { device: Device };
+
+export default function DeviceCard({ device }: Props) {
     const [expanded, setExpanded] = React.useState(false);
 
     const handleExpandClick = () => {
@@ -38,79 +41,94 @@ export default function DeviceCard() {
     };
 
     return (
-        <Card sx={{ maxWidth: 345 }}>
+        <Card>
             <CardHeader
                 avatar={
                     <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                        R
+                        <DeviceHubIcon />
                     </Avatar>
                 }
                 action={
                     <IconButton aria-label="settings">
-                        <MoreVertIcon />
+                        <Link href={"/owners/" + device.id + "/edit"}>
+                            <EditIcon />
+                        </Link>
                     </IconButton>
                 }
-                title="Shrimp and Chorizo Paella"
-                subheader="September 14, 2016"
+                title={device.device_name}
+                subheader={device.device_type}
             />
-            <CardMedia
-                component="img"
-                height="194"
-                image="/static/images/cards/paella.jpg"
-                alt="Paella dish"
+            <CardHeader
+                avatar={
+                    device.owned ? (
+                        <>
+                            <Typography align="center" variant="subtitle1">
+                                Owner:{" " + device.owner_name}
+                                <IconButton aria-label="owner-profile">
+                                    <Link href={"/owners/" + device.owner_id}>
+                                        <AccountCircleIcon />
+                                    </Link>
+                                </IconButton>
+                            </Typography>
+                        </>
+                    ) : (
+                        <>
+                            <Typography align="center" variant="subtitle1">
+                                assign to an owner
+                                <IconButton aria-label="owner-profile">
+                                    <Link
+                                        href={"/devices/" + device.id + "/edit"}
+                                    >
+                                        <AddCircleIcon />
+                                    </Link>
+                                </IconButton>
+                            </Typography>
+                        </>
+                    )
+                }
+                action={
+                    <Typography align="center" variant="subtitle1">
+                        Available space: {device.available_space}Mb
+                    </Typography>
+                }
+                title={
+                    <Typography align="center" variant="subtitle1">
+                        Batery: {device.battery}%
+                    </Typography>
+                }
             />
-            <CardContent>
-                <Typography variant="body2" color="text.secondary">
-                    This impressive paella is a perfect party dish and a fun
-                    meal to cook together with your guests. Add 1 cup of frozen
-                    peas along with the mussels, if you like.
+
+            {/* <CardContent>
+                {device.owned ? (
+                    <>
+                        <Typography align="left" variant="subtitle1">
+                            Owner:{" " + device.owner_name}
+                            <IconButton aria-label="owner-profile">
+                                <Link href={"/owners/" + device.owner_id}>
+                                    <AccountCircleIcon />
+                                </Link>
+                            </IconButton>
+                        </Typography>
+                    </>
+                ) : (
+                    <>
+                        <Typography align="left" variant="subtitle1">
+                            assign to an owner
+                            <IconButton aria-label="owner-profile">
+                                <Link href={"/owners/" + device.id + "/edit"}>
+                                    <AddCircleIcon />
+                                </Link>
+                            </IconButton>
+                        </Typography>
+                    </>
+                )}
+                <Typography align="left" variant="subtitle1">
+                    Batery: {device.battery}%
                 </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
-                </IconButton>
-                <IconButton aria-label="share">
-                    <ShareIcon />
-                </IconButton>
-                <ExpandMore
-                    expand={expanded}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                    aria-label="show more"
-                >
-                    <ExpandMoreIcon />
-                </ExpandMore>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                    <Typography paragraph>Method:</Typography>
-                    <Typography paragraph>
-                        Heat 1/2 cup of the broth in a pot until simmering, add
-                        saffron and set aside for 10 minutes.
-                    </Typography>
-                    <Typography paragraph>
-                        Heat oil in a (14- to 16-inch) paella pan or a large,
-                        deep skillet over medium-high heat. Add chicken, shrimp
-                        and chorizo, and cook, stirring occasionally until
-                        lightly browned, 6 to 8 minutes.
-                    </Typography>
-                    <Typography paragraph>
-                        Add rice and stir very gently to distribute. Top with
-                        artichokes and peppers, and cook without stirring, until
-                        most of the liquid is absorbed, 15 to 18 minutes. Reduce
-                        heat to medium-low, add reserved shrimp and mussels,
-                        tucking them down into the rice, and cook again without
-                        stirring, until mussels have opened and rice is just
-                        tender, 5 to 7 minutes more. (Discard any mussels that
-                        don&apos;t open.)
-                    </Typography>
-                    <Typography>
-                        Set aside off of the heat to let rest for 10 minutes,
-                        and then serve.
-                    </Typography>
-                </CardContent>
-            </Collapse>
+                <Typography align="left" variant="subtitle1">
+                    Available space: {device.available_space}Mb
+                </Typography>
+            </CardContent> */}
         </Card>
     );
 }
