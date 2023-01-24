845c142c02422890cee2e47c4a5d232ede377b1e
Rebeca Kipper <rebecakipper@gmail.com>
2023-01-24 10:22:26 +0100

    updates home page layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,59 @@
 import Link from "next/link";
-import DeviceCardList from "@/components/DeviceCardList";
+import DeviceCard from "@/components/DeviceCard";
+import type { Device } from "@/types";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
 
-let listOfDevices = [
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+}));
+
+let listOfDevices: Device[] = [
     {
         id: 5,
-        name: "device A",
-        type: "laptop",
-        ownerId: 2,
-        availableSpace: 50,
-        battery: 89,
+        device_name: "device A",
+        device_type: "Laptop",
+        available_space: 32,
+        owned: true,
+        battery: 100,
+        owner_name: "Bob",
+        owner_id: 1,
     },
     {
         id: 2,
-        name: "device B",
-        type: "laptop",
-        ownerId: 3,
-        availableSpace: 7,
+        device_name: "device B",
+        device_type: "Tablet",
+        available_space: 876,
+        owned: false,
         battery: 55,
+        owner_name: null,
+        owner_id: null,
     },
 ];
 
 function HomePage() {
     return (
         <>
-            <DeviceCardList devices={listOfDevices}></DeviceCardList>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2}>
+                    {listOfDevices.map((deviceObj) => {
+                        return (
+                            <Grid item xs={4} key={deviceObj.id}>
+                                <Item>
+                                    <DeviceCard device={deviceObj}></DeviceCard>
+                                </Item>
+                            </Grid>
+                        );
+                    })}
+                </Grid>
+            </Box>
         </>
     );
 }
