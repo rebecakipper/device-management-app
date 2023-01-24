a33fc2e64632f8b1bed6213f7e29df383521dfaf
Rebeca Kipper <rebecakipper@gmail.com>
2023-01-24 10:22:52 +0100

    connects db to BE

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { Client } from "pg";
+const client = new Client();
+
+client.connect();
+
+client.query("SELECT * FROM owners;", (err, res) => {
+    console.log(err ? err.stack : res.rows);
+    client.end();
+});
 
 type Data = {
-  name: string
-}
+    name: string;
+};
 
 export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+    res.status(200).json({ name: "John Doe" });
 }
