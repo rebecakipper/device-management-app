1bc3cd6682a2086e7e900e6b6a39479c9026bf86
Rebeca Kipper <rebecakipper@gmail.com>
2023-01-24 17:19:09 +0100

    redirects to devices page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    async redirects() {
+        return [
+            {
+                source: "/",
+                destination: "/devices",
+                permanent: false,
+            },
+        ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
